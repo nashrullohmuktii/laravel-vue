b9d49f0cbccf7369352b1b95831b686dd1779320
Dio Maulana <maulanadiodm@gmail.com>
2021-12-15 19:36:32 +0700

    task simple query bag 1

--- diomaulana/Review PHP dan MySQL/apotek.sql
+++ diomaulana/Review PHP dan MySQL/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Des 2021 pada 20.21
+-- Waktu pembuatan: 15 Des 2021 pada 12.47
 -- Versi server: 10.4.19-MariaDB
 -- Versi PHP: 8.0.7
 
@@ -40,7 +40,15 @@ CREATE TABLE `apoteker` (
 
 INSERT INTO `apoteker` (`id`, `name`, `umur`, `alamat`) VALUES
 (1, 'Dio Maulana', 25, 'Padang'),
-(2, 'Jhon Doe', 22, 'Jakarta');
+(2, 'Jhon Doe', 22, 'Jakarta'),
+(4, 'Ani', 20, 'Cilacap'),
+(5, 'Budi', 18, 'kampung Duren'),
+(6, 'Vina', 29, 'Cianjur'),
+(7, 'Aristoteles', 23, 'Jakarta'),
+(8, 'Brian', 27, 'Bandung'),
+(9, 'Agus', 22, 'Cimahi'),
+(10, 'Zian', 25, 'Siteba'),
+(11, 'Rani', 25, 'Bogor');
 
 -- --------------------------------------------------------
 
@@ -60,7 +68,15 @@ CREATE TABLE `obat` (
 
 INSERT INTO `obat` (`id`, `nama_obat`, `expire_obat`) VALUES
 (1, 'Paracetamol', '2021-12-14 20:15:18'),
-(2, 'Dumin', '2024-12-26 02:15:18');
+(2, 'Dumin', '2024-12-26 02:15:18'),
+(3, 'Vitamin C', '2024-12-18 18:33:52'),
+(4, 'Vitamin D', '2024-12-26 18:33:52'),
+(5, 'Entrostop', '2023-12-25 18:33:52'),
+(6, 'Paramex', '2022-12-27 18:33:52'),
+(7, 'Bodrex', '2026-12-27 18:33:52'),
+(8, 'Ampicillin', '2025-12-31 18:33:52'),
+(9, 'Antropin', '2026-12-30 18:33:52'),
+(10, 'estazolam', '2024-12-19 18:33:52');
 
 -- --------------------------------------------------------
 
@@ -82,7 +98,15 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`id`, `id_obat`, `price`, `date_order`, `id_apoteker`) VALUES
 (1, 2, 20000, '2021-12-14 20:20:25', 1),
-(2, 1, 12000, '2021-12-14 20:20:25', 2);
+(2, 1, 12000, '2021-12-14 20:20:25', 2),
+(3, 9, 50000, '2020-12-22 18:37:36', 11),
+(4, 6, 31000, '2021-09-01 18:37:36', 8),
+(5, 2, 40000, '2018-12-13 18:37:36', 2),
+(6, 3, 30000, '2020-12-01 18:37:36', 1),
+(7, 9, 31200, '2021-09-01 18:37:36', 7),
+(8, 9, 30000, '2021-12-01 18:37:36', 6),
+(9, 4, 60000, '2020-10-14 18:37:36', 10),
+(10, 7, 32220, '2021-02-01 18:37:36', 11);
 
 --
 -- Indexes for dumped tables
@@ -116,19 +140,19 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT untuk tabel `apoteker`
 --
 ALTER TABLE `apoteker`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
