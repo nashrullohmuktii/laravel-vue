d43392505f65bb8750d110e60320592cadf11a80
doniarifin <muh.dhoni@gmail.com>
2021-12-16 06:40:21 +0700

    task tipe data, primarykey, foreignkey

--- ReviewPHPdanMySql/apotek.sql
+++ ReviewPHPdanMySql/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 02:19 PM
+-- Generation Time: Dec 15, 2021 at 04:57 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
