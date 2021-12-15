471759f85cd39716b246ed9c1a3f5b2948107d85
Fahri Gunawan <fahrigunawan2323@gmail.com>
2021-12-15 11:06:29 +0700

    update db_apotek_fahri.sql

--- fahri-gunawan/Review-PHP-dan-MySQL/Tipe-data-mysql-primarykey-foreignkey/db_apotek_fahri.sql
+++ fahri-gunawan/Review-PHP-dan-MySQL/Tipe-data-mysql-primarykey-foreignkey/db_apotek_fahri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 14 Des 2021 pada 18.03
+-- Waktu pembuatan: 15 Des 2021 pada 05.05
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.4.25
 
@@ -34,6 +34,22 @@ CREATE TABLE `obat` (
   `stok` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama_obat`, `harga_obat`, `stok`) VALUES
+(1, 'Aspirin', 3000, 300),
+(2, 'Ibuprofen', 3500, 250),
+(3, 'Acetaminophen', 7000, 700),
+(4, 'Indomethacin', 6000, 500),
+(5, 'Sumatriptan', 6500, 230),
+(6, 'Ketorolac', 4500, 670),
+(7, 'Zolmitriptan', 8888, 666),
+(8, 'Naproxen', 2500, 50),
+(9, 'Benzocaine', 6000, 300),
+(10, 'Eritromisin', 3500, 250);
+
 -- --------------------------------------------------------
 
 --
@@ -48,6 +64,22 @@ CREATE TABLE `pelanggan` (
   `jenis_kelamin` enum('pria','wanita') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `nama_pelanggan`, `umur`, `alamat`, `jenis_kelamin`) VALUES
+(1, 'Fahri Gunawan', 23, 'Jl. Pertahanan Komp. Griya Pertahanan 3', 'pria'),
+(2, 'Gunawan Fahri', 23, 'Jl. Pertahanan Komp. Griya Pertahanan 3 Plaju', 'pria'),
+(3, 'Tio Dinata', 20, 'Palembang', 'pria'),
+(4, 'Zahra Tiara', 22, 'Palembang', 'wanita'),
+(5, 'Juminah', 53, 'Palembang', 'wanita'),
+(6, 'Supriadi', 43, 'Plaju Palembang', 'pria'),
+(7, 'Rahman', 30, 'Sumatera Selatan', 'pria'),
+(8, 'Bahtiar', 45, 'Sumatera Utara', 'pria'),
+(9, 'Rosdiana', 50, 'Sumsel Palembang', 'wanita'),
+(10, 'Fah Gun', 23, 'Palembang', 'pria');
+
 -- --------------------------------------------------------
 
 --
@@ -55,13 +87,29 @@ CREATE TABLE `pelanggan` (
 --
 
 CREATE TABLE `penjualan` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_penjualan` int(11) NOT NULL,
   `id_pelanggan` int(11) NOT NULL,
   `tgl_transaksi` date NOT NULL,
   `total_transaksi` int(10) NOT NULL,
   `total_bayar` int(10) NOT NULL,
   `total_kembalian` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_pelanggan`, `tgl_transaksi`, `total_transaksi`, `total_bayar`, `total_kembalian`) VALUES
+(1, 4, '2021-12-09', 15000, 20000, 5000),
+(2, 1, '2021-12-14', 45000, 100000, 55000),
+(3, 3, '2021-12-03', 30000, 50000, 20000),
+(4, 5, '2021-12-15', 200000, 200000, 0),
+(5, 6, '2021-12-10', 50000, 100000, 50000),
+(6, 9, '2021-12-01', 70000, 100000, 30000),
+(7, 8, '2021-12-02', 60000, 100000, 40000),
+(8, 2, '2021-12-08', 20000, 50000, 30000),
+(9, 1, '2021-12-15', 10000, 20000, 10000),
+(10, 10, '2021-12-15', 35000, 50000, 15000);
 
 -- --------------------------------------------------------
 
@@ -71,10 +119,26 @@ CREATE TABLE `penjualan` (
 
 CREATE TABLE `penjualan_detail` (
   `id_transaksi_detail` int(11) NOT NULL,
-  `id_transaksi` int(11) NOT NULL,
+  `id_penjualan` int(11) NOT NULL,
   `id_obat` int(11) NOT NULL,
   `jumlah_obat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penjualan_detail`
+--
+
+INSERT INTO `penjualan_detail` (`id_transaksi_detail`, `id_penjualan`, `id_obat`, `jumlah_obat`) VALUES
+(1, 5, 7, 22),
+(2, 2, 6, 6),
+(3, 6, 5, 5),
+(4, 3, 8, 7),
+(5, 2, 5, 9),
+(6, 4, 4, 4),
+(7, 5, 2, 5),
+(8, 1, 7, 5),
+(9, 3, 8, 2),
+(10, 10, 10, 66);
 
 --
 -- Indexes for dumped tables
@@ -96,7 +160,7 @@ ALTER TABLE `pelanggan`
 -- Indeks untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
-  ADD PRIMARY KEY (`id_transaksi`),
+  ADD PRIMARY KEY (`id_penjualan`),
   ADD KEY `fk_pelanggan` (`id_pelanggan`);
 
 --
@@ -104,7 +168,7 @@ ALTER TABLE `penjualan`
 --
 ALTER TABLE `penjualan_detail`
   ADD PRIMARY KEY (`id_transaksi_detail`),
-  ADD KEY `fk_transaksi` (`id_transaksi`),
+  ADD KEY `fk_transaksi` (`id_penjualan`),
   ADD KEY `fk_obat` (`id_obat`);
 
 --
@@ -115,25 +179,25 @@ ALTER TABLE `penjualan_detail`
 -- AUTO_INCREMENT untuk tabel `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `penjualan`
 --
 ALTER TABLE `penjualan`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `penjualan_detail`
 --
 ALTER TABLE `penjualan_detail`
-  MODIFY `id_transaksi_detail` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -150,7 +214,7 @@ ALTER TABLE `penjualan`
 --
 ALTER TABLE `penjualan_detail`
   ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`),
-  ADD CONSTRAINT `fk_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `penjualan` (`id_transaksi`);
+  ADD CONSTRAINT `fk_transaksi` FOREIGN KEY (`id_penjualan`) REFERENCES `penjualan` (`id_penjualan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
