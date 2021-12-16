cb346901f2317e88c3a535144fee2dda7580775a
doniarifin <muh.dhoni@gmail.com>
2021-12-16 16:30:26 +0700

    revisi task tipedata dan relasi database

--- ReviewPHPdanMySql/apotek.sql
+++ ReviewPHPdanMySql/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2021 at 04:57 PM
+-- Generation Time: Dec 16, 2021 at 10:22 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -24,27 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bentuk`
+-- Table structure for table `detail_orders`
 --
 
-CREATE TABLE `bentuk` (
+CREATE TABLE `detail_orders` (
   `id` int(11) NOT NULL,
-  `bentuk` varchar(32) NOT NULL,
-  `harga` varchar(32) NOT NULL,
-  `expired` date NOT NULL,
-  `id_petunjuk_pemakaian` int(11) NOT NULL
+  `id_pelanggan` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `harga` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `bentuk`
+-- Dumping data for table `detail_orders`
 --
 
-INSERT INTO `bentuk` (`id`, `bentuk`, `harga`, `expired`, `id_petunjuk_pemakaian`) VALUES
-(9, 'kapsul', 'Rp12000', '2023-12-15', 1),
-(10, 'sirup', 'Rp15000', '2023-12-15', 2),
-(11, 'kaplet', 'Rp5000', '2022-03-18', 4),
-(12, 'tablet', 'Rp5000', '2022-05-13', 3),
-(13, 'sirup', 'Rp12000', '2024-03-22', 4);
+INSERT INTO `detail_orders` (`id`, `id_pelanggan`, `id_obat`, `harga`) VALUES
+(1, 1, 5, 'Rp2000'),
+(2, 3, 1, 'Rp5000'),
+(3, 2, 6, 'Rp12000'),
+(4, 4, 2, 'Rp13000'),
+(5, 5, 3, 'Rp6000'),
+(6, 1, 2, 'Rp13000'),
+(7, 4, 5, 'Rp2000');
 
 -- --------------------------------------------------------
 
@@ -54,70 +55,99 @@ INSERT INTO `bentuk` (`id`, `bentuk`, `harga`, `expired`, `id_petunjuk_pemakaian
 
 CREATE TABLE `obat` (
   `id` int(11) NOT NULL,
-  `nama` varchar(32) NOT NULL,
-  `bahan_aktif` varchar(108) NOT NULL,
-  `manfaat` text NOT NULL,
-  `dikonsumsi_oleh` text NOT NULL,
-  `id_bentuk` int(11) NOT NULL
+  `nama` varchar(64) NOT NULL,
+  `stok` varchar(12) NOT NULL,
+  `expired` date NOT NULL,
+  `id_petunjuk_pemakaian` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `obat`
 --
 
-INSERT INTO `obat` (`id`, `nama`, `bahan_aktif`, `manfaat`, `dikonsumsi_oleh`, `id_bentuk`) VALUES
-(1, 'Panadol', 'paracetamol 500mg', 'meredakan demam, sakit kepala, s', '> usia 15 tahun', 9),
-(2, 'Tempra sirup', 'paracetamol 250mg', 'meredakan demam, sakit kepala, dan nyeri untuk anak-anak', '> usia 1 tahun', 10),
-(4, 'Procold', 'Paracetamol 500 mg, Phenylephrine HCl 10 mg.', 'meringankan gejala flu seperti demam, sakit kepala, hidung tersumbat, dan batuk tidak berdahak', '> usia 15 tahun', 11),
-(6, 'Promag ', 'hydrotalcite 200mg, magnesium hidroksida 150mg', 'Mengatasi sakit maag, GERD, dan perut kembung', '> usia 9 tahun', 12),
-(7, 'Siladex', 'Bromhexine HCl 10 mg, Guaifenesin 50 mg\r\n', 'Meredakan batuk berdahak dan mempermudah pengeluaran dahak.', '> usia 9 tahun', 10);
+INSERT INTO `obat` (`id`, `nama`, `stok`, `expired`, `id_petunjuk_pemakaian`) VALUES
+(1, 'Panadol', '50', '2023-12-15', 1),
+(2, 'Siladex flu dan batuk', '70', '2024-03-22', 2),
+(3, 'Konimex sakit kepala', '40', '2022-05-13', 2),
+(4, 'Oskadon', '50', '2022-07-07', 2),
+(5, 'Komix', '100', '2022-05-13', 1),
+(6, 'Mylanta sirup', '27', '2022-05-13', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `petunjuk`
+-- Table structure for table `pelanggan`
 --
 
-CREATE TABLE `petunjuk` (
+CREATE TABLE `pelanggan` (
   `id` int(11) NOT NULL,
-  `usia 5-9` varchar(64) DEFAULT NULL,
-  `usia 9-15` varchar(64) DEFAULT NULL,
-  `usia 15 keatas` varchar(64) DEFAULT NULL
+  `nama_pelanggan` varchar(100) NOT NULL,
+  `alamat` text NOT NULL,
+  `usia` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `petunjuk`
+-- Dumping data for table `pelanggan`
 --
 
-INSERT INTO `petunjuk` (`id`, `usia 5-9`, `usia 9-15`, `usia 15 keatas`) VALUES
-(1, NULL, NULL, '3x2 sehari'),
-(2, '2 x sehari 1 sendok takar (5ml)', '3 x sehari 2 sendok takar (10ml)', NULL),
-(3, NULL, '2x1 sehari sebelum makan', '3x1 sehari sebelum makan'),
-(4, NULL, NULL, '3x1 sehari setelah makan'),
-(5, NULL, NULL, '3x2 sendok teh sehari');
+INSERT INTO `pelanggan` (`id`, `nama_pelanggan`, `alamat`, `usia`) VALUES
+(1, 'Ahmad Shobari', 'Jl. Manggar No. 02, Surabaya', '18'),
+(2, 'Nurhalimah', 'Jl. Merak No. 35, Surabaya', '20'),
+(3, 'Alex Nurmustofa', 'Jl. Kenanga, No.56, Sidoarjo', '22'),
+(4, 'Rini Setiawati', 'Jl. Pandjaitan, No. 27, Gresik', '24'),
+(5, 'Rahmat Pahlevi', 'Jl. Ahmad Yani, No.24 Surabaya', '20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petunjuk_pemakaian`
+--
+
+CREATE TABLE `petunjuk_pemakaian` (
+  `id` int(11) NOT NULL,
+  `anak-anak` varchar(100) DEFAULT NULL,
+  `remaja` varchar(100) DEFAULT NULL,
+  `dewasa` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `petunjuk_pemakaian`
+--
+
+INSERT INTO `petunjuk_pemakaian` (`id`, `anak-anak`, `remaja`, `dewasa`) VALUES
+(1, '1x1 sehari', '2x1 sehari', '3x1 sehari'),
+(2, '', '2x1 sendok makan sehari', '3x1 sendok makan sehari'),
+(3, '2x2 sehari', '3x2 sehari', '3x2 sehari');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bentuk`
+-- Indexes for table `detail_orders`
 --
-ALTER TABLE `bentuk`
+ALTER TABLE `detail_orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_petunjuk` (`id_petunjuk_pemakaian`);
+  ADD KEY `fk_obat` (`id_obat`),
+  ADD KEY `fk_pelanggan` (`id_pelanggan`);
 
 --
 -- Indexes for table `obat`
 --
 ALTER TABLE `obat`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_bentuk` (`id_bentuk`);
+  ADD KEY `fk_petunjuk_pemakaian` (`id_petunjuk_pemakaian`);
 
 --
--- Indexes for table `petunjuk`
+-- Indexes for table `pelanggan`
 --
-ALTER TABLE `petunjuk`
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `petunjuk_pemakaian`
+--
+ALTER TABLE `petunjuk_pemakaian`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -125,38 +155,45 @@ ALTER TABLE `petunjuk`
 --
 
 --
--- AUTO_INCREMENT for table `bentuk`
+-- AUTO_INCREMENT for table `detail_orders`
 --
-ALTER TABLE `bentuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `detail_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `petunjuk`
+-- AUTO_INCREMENT for table `pelanggan`
 --
-ALTER TABLE `petunjuk`
+ALTER TABLE `pelanggan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `petunjuk_pemakaian`
+--
+ALTER TABLE `petunjuk_pemakaian`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `bentuk`
+-- Constraints for table `detail_orders`
 --
-ALTER TABLE `bentuk`
-  ADD CONSTRAINT `fk_petunjuk` FOREIGN KEY (`id_petunjuk_pemakaian`) REFERENCES `petunjuk` (`id`);
+ALTER TABLE `detail_orders`
+  ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_pelanggan` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id`);
 
 --
 -- Constraints for table `obat`
 --
 ALTER TABLE `obat`
-  ADD CONSTRAINT `fk_bentuk` FOREIGN KEY (`id_bentuk`) REFERENCES `bentuk` (`id`);
+  ADD CONSTRAINT `fk_petunjuk_pemakaian` FOREIGN KEY (`id_petunjuk_pemakaian`) REFERENCES `petunjuk_pemakaian` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
