a3371d5e3588a130c47112f10abb80d482d0a872
unknown <omar.mochtr@gmail.com>
2021-12-18 00:04:56 +0700

    task tipe data mysql

--- omar/review php dan mysql/apotek.sql
+++ omar/review php dan mysql/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2021 at 04:43 PM
+-- Generation Time: Dec 17, 2021 at 06:03 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -48,12 +48,36 @@ INSERT INTO `karyawan` (`id`, `nama`, `jabatan`, `no_telpon`, `alamat`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `nama_produk`
+--
+
+CREATE TABLE `nama_produk` (
+  `id` int(11) NOT NULL,
+  `nama_produk` varchar(32) NOT NULL,
+  `izin_bpom` varchar(15) NOT NULL,
+  `perusahaan_farmasi` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `nama_produk`
+--
+
+INSERT INTO `nama_produk` (`id`, `nama_produk`, `izin_bpom`, `perusahaan_farmasi`) VALUES
+(1, 'Obat Sakit Kepala', 'ab12345', 'PT. Obat Hebat'),
+(2, 'Obat Migraib', 'zb1235', 'PT. Obat Hebat'),
+(3, 'Obat Migrain', 'zb1235', 'PT. Obat Hebat'),
+(4, 'Obat Flu', 'zb1236', 'PT. Obat Tua'),
+(5, 'Obat Batuk', 'zb2235', 'PT. Obat Obat');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `stok_masuk`
 --
 
 CREATE TABLE `stok_masuk` (
   `id` int(11) NOT NULL,
-  `nama_produk` varchar(64) NOT NULL,
+  `id_nama_produk` int(11) NOT NULL,
   `harga` int(10) NOT NULL,
   `tanggal_stok_masuk` date NOT NULL,
   `stok_masuk` int(11) NOT NULL,
@@ -64,8 +88,12 @@ CREATE TABLE `stok_masuk` (
 -- Dumping data for table `stok_masuk`
 --
 
-INSERT INTO `stok_masuk` (`id`, `nama_produk`, `harga`, `tanggal_stok_masuk`, `stok_masuk`, `id_penanggung_jawab`) VALUES
-(1, 'obat sakit kepala', 13000, '2021-12-01', 100, 4);
+INSERT INTO `stok_masuk` (`id`, `id_nama_produk`, `harga`, `tanggal_stok_masuk`, `stok_masuk`, `id_penanggung_jawab`) VALUES
+(2, 1, 13000, '2021-12-01', 100, 4),
+(3, 3, 15000, '2021-12-01', 120, 4),
+(4, 3, 15000, '2021-12-01', 120, 4),
+(5, 5, 11000, '2021-12-01', 90, 4),
+(6, 1, 13000, '2021-11-03', 110, 1);
 
 --
 -- Indexes for dumped tables
@@ -78,11 +106,18 @@ ALTER TABLE `karyawan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `nama_produk`
+--
+ALTER TABLE `nama_produk`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `stok_masuk`
 --
 ALTER TABLE `stok_masuk`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_pp` (`id_penanggung_jawab`);
+  ADD KEY `fk_pp` (`id_penanggung_jawab`),
+  ADD KEY `fk_produk` (`id_nama_produk`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -95,10 +130,16 @@ ALTER TABLE `karyawan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT for table `nama_produk`
+--
+ALTER TABLE `nama_produk`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `stok_masuk`
 --
 ALTER TABLE `stok_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -108,7 +149,8 @@ ALTER TABLE `stok_masuk`
 -- Constraints for table `stok_masuk`
 --
 ALTER TABLE `stok_masuk`
-  ADD CONSTRAINT `fk_pp` FOREIGN KEY (`id_penanggung_jawab`) REFERENCES `karyawan` (`id`);
+  ADD CONSTRAINT `fk_pp` FOREIGN KEY (`id_penanggung_jawab`) REFERENCES `karyawan` (`id`),
+  ADD CONSTRAINT `fk_produk` FOREIGN KEY (`id_nama_produk`) REFERENCES `nama_produk` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
