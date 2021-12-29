6178649cb595055b1905f3cbe4b47810136fd217
Azizs2 <sy.aziz2505@gmail.com>
2021-12-29 19:02:03 +0700

    Updated untuk join multiple table

--- Aziz/apotek.sql
+++ Aziz/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2021 at 07:03 AM
+-- Generation Time: Dec 29, 2021 at 12:58 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -40,16 +40,15 @@ CREATE TABLE `tb_dokter` (
 --
 
 INSERT INTO `tb_dokter` (`id_dokter`, `nama_dokter`, `tanggal_lahir`, `no_telfon`, `spesialis`) VALUES
-(1, 'Aziz', '1999-01-26', '85647555528', 'gigi'),
-(3, 'Ani', '1999-01-26', '85647555528', 'jantung'),
-(4, 'Nisa', '1990-01-27', '85647555528', 'kulit'),
-(5, 'Dila', '1988-01-28', '85647555528', 'gigi'),
-(6, 'Nila', '1989-01-25', '85647555528', 'jantung'),
-(7, 'Ida', '1999-01-26', '85647555528', 'kulit'),
-(8, 'Idaa', '1990-01-27', '85647555528', 'gigi'),
-(9, 'anjar', '1988-01-28', '85647555528', 'jantung'),
-(10, 'anjas', '1996-02-02', '85647555528', 'kulit'),
-(11, 'lutfi', '1995-02-03', '85647555528', 'gigi');
+(1, 'Aziz', '1999-01-26', '851234567890', 'hati'),
+(3, 'Ani', '1999-01-26', '851234567890', 'jantung'),
+(4, 'Nisa', '1990-01-27', '851234567890', 'kulit'),
+(5, 'Dila', '1988-01-28', '851234567890', 'gigi'),
+(6, 'Nila', '1989-01-25', '851234567890', 'jantung'),
+(7, 'Ida', '1999-01-26', '851234567890', 'kulit'),
+(8, 'Idaa', '1990-01-27', '851234567890', 'gigi'),
+(10, 'anjas', '1996-02-02', '851234567890', 'kulit'),
+(11, 'lutfi', '1995-02-03', '851234567890', 'gigi');
 
 -- --------------------------------------------------------
 
@@ -89,6 +88,35 @@ INSERT INTO `tb_obat` (`id_obat`, `nama_obat`, `tanggal_pembuat`, `tanggal_kadal
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_pasien`
+--
+
+CREATE TABLE `tb_pasien` (
+  `id_pasien` int(11) NOT NULL,
+  `nama_pasien` varchar(64) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telfon` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pasien`
+--
+
+INSERT INTO `tb_pasien` (`id_pasien`, `nama_pasien`, `tanggal_lahir`, `alamat`, `no_telfon`) VALUES
+(1, 'Lukman', '2021-12-01', 'Kota Depok', '085647555528'),
+(3, 'aryo', '2021-09-07', 'kota jakarta', '85647555528'),
+(11, 'aryo', '2000-01-02', 'kota jakarta', '85647555528'),
+(12, 'johan', '2001-01-03', 'kota bekasi', '85647555528'),
+(13, 'ari', '1999-01-01', 'kota bogor', '85647555528'),
+(14, 'reza', '2000-01-02', 'kota depok', '85647555528'),
+(15, 'hilda', '2001-01-03', 'kota jakarta', '85647555528'),
+(16, 'harry', '1999-01-01', 'kota bekasi', '85647555528'),
+(17, 'jeny', '2000-01-02', 'kota bogor', '85647555528');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_resep`
 --
 
@@ -98,24 +126,25 @@ CREATE TABLE `tb_resep` (
   `tanggal` date NOT NULL,
   `jenis_resep` text NOT NULL,
   `id_obat` int(11) DEFAULT NULL,
-  `id_dokter` int(11) DEFAULT NULL
+  `id_dokter` int(11) DEFAULT NULL,
+  `id_pasien` int(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_resep`
 --
 
-INSERT INTO `tb_resep` (`id_resep`, `nama`, `tanggal`, `jenis_resep`, `id_obat`, `id_dokter`) VALUES
-(3, 'penurun panas', '2021-12-21', 'vitamin C', 15, 11),
-(6, 'penambah napsu makan', '2021-12-22', 'Vitamin D', 12, 5),
-(7, 'antibody', '2021-12-23', 'Vitamin C', 15, 10),
-(8, 'kebugaran', '2021-12-21', 'vitamin B1', 3, 8),
-(9, 'untuk mata', '2021-12-22', 'vitamin A', 1, 11),
-(10, 'penurun panas', '2021-12-23', 'Vitamin C', 15, 4),
-(11, 'untuk mata', '2021-12-22', 'vitamin A', 1, 11),
-(12, 'antibody', '2021-12-23', 'Vitamin C', 15, 10),
-(13, 'kebugaran', '2021-12-21', 'vitamin B1', 3, 8),
-(14, 'penurun panas', '2021-12-21', 'vitamin C', 15, 11);
+INSERT INTO `tb_resep` (`id_resep`, `nama`, `tanggal`, `jenis_resep`, `id_obat`, `id_dokter`, `id_pasien`) VALUES
+(3, 'penurun panas', '2021-12-21', 'vitamin C', 15, 11, 15),
+(6, 'penambah napsu makan', '2021-12-22', 'Vitamin D', 12, 5, 3),
+(7, 'antibody', '2021-12-23', 'Vitamin C', 15, 10, 13),
+(8, 'kebugaran', '2021-12-21', 'vitamin B1', 3, 8, 11),
+(9, 'untuk mata', '2021-12-22', 'vitamin A', 1, 11, 15),
+(10, 'penurun panas', '2021-12-23', 'Vitamin C', 15, 4, 1),
+(11, 'untuk mata', '2021-12-22', 'vitamin A', 1, 11, 17),
+(12, 'antibody', '2021-12-23', 'Vitamin C', 15, 10, 14),
+(13, 'kebugaran', '2021-12-21', 'vitamin B1', 3, 8, 12),
+(14, 'penurun panas', '2021-12-21', 'vitamin C', 15, 11, 17);
 
 --
 -- Indexes for dumped tables
@@ -134,13 +163,21 @@ ALTER TABLE `tb_obat`
   ADD PRIMARY KEY (`id_obat`);
 
 --
+-- Indexes for table `tb_pasien`
+--
+ALTER TABLE `tb_pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
 -- Indexes for table `tb_resep`
 --
 ALTER TABLE `tb_resep`
   ADD PRIMARY KEY (`id_resep`),
   ADD KEY `id_obat` (`id_obat`),
   ADD KEY `id_dokter` (`id_dokter`),
-  ADD KEY `id_obat_2` (`id_obat`);
+  ADD KEY `id_obat_2` (`id_obat`),
+  ADD KEY `id_pasien` (`id_pasien`),
+  ADD KEY `id_pasien_2` (`id_pasien`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -159,6 +196,12 @@ ALTER TABLE `tb_obat`
   MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
+-- AUTO_INCREMENT for table `tb_pasien`
+--
+ALTER TABLE `tb_pasien`
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
 -- AUTO_INCREMENT for table `tb_resep`
 --
 ALTER TABLE `tb_resep`
@@ -173,7 +216,8 @@ ALTER TABLE `tb_resep`
 --
 ALTER TABLE `tb_resep`
   ADD CONSTRAINT `tb_resep_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `tb_obat` (`id_obat`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tb_resep_ibfk_3` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `tb_resep_ibfk_3` FOREIGN KEY (`id_dokter`) REFERENCES `tb_dokter` (`id_dokter`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_resep_ibfk_4` FOREIGN KEY (`id_pasien`) REFERENCES `tb_pasien` (`id_pasien`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
