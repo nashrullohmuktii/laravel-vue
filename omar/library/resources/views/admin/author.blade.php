a6611be1af4026c9033ef092d72c71184d19b414
unknown <omar.mochtr@gmail.com>
2022-01-10 17:22:35 +0700

    Tugas CRUD VUE.js part2

--- omar/library/resources/views/admin/author.blade.php
+++ omar/library/resources/views/admin/author.blade.php
@@ -5,7 +5,10 @@
 
 
 @section('css')
-    
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
 @endsection
 
 
@@ -33,7 +36,7 @@
       </div> --}}
       <!-- /.card-header -->
       <div class="card-body p-0 mt-2 mb-2 mr-3">
-          <table class="table table-bordered mr-2 ml-2">
+          <table id="table" class="table table-bordered table-striped mr-2 ml-2">
           <thead>
             <tr class="text-center">
               <th style="width: 10px">No</th>
@@ -124,6 +127,41 @@
 @endsection
 
 @section('js')
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+<!-- Page specific script -->
+<script type="text/javascript">
+  $(function () {
+    $("#table").DataTable();
+    // $("#table").DataTable({
+    //   "responsive": true, "lengthChange": false, "autoWidth": false,
+    //   "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    // }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    // $('#table').DataTable({
+    //   "paging": true,
+    //   "lengthChange": false,
+    //   "searching": false,
+    //   "ordering": true,
+    //   "info": true,
+    //   "autoWidth": false,
+    //   "responsive": true,
+    // });
+  });
+</script>
+
+{{-- CRUD VUE js --}}
     <script type="text/javascript">
       var controller = new Vue ({
           el: '#controller',
