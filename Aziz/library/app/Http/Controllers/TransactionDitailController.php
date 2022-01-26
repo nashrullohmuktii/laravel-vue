84ed7eca1c306bb46b9e637bee6b89f6538f6924
Azizs2 <sy.aziz2505@gmail.com>
2022-01-26 16:45:28 +0700

    add controller

---
+++ Aziz/library/app/Http/Controllers/TransactionDitailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TransactionDitail;
+use Illuminate\Http\Request;
+
+class TransactionDitailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TransactionDitail  $transactionDitail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TransactionDitail $transactionDitail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TransactionDitail  $transactionDitail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TransactionDitail $transactionDitail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TransactionDitail  $transactionDitail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TransactionDitail $transactionDitail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TransactionDitail  $transactionDitail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TransactionDitail $transactionDitail)
+    {
+        //
+    }
+}
