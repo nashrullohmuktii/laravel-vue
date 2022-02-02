71158eaf1eea92ebeb462944c68741bf9fe2df9a
doniarifin <muh.dhoni@gmail.com>
2022-02-02 18:34:14 +0700

    task eloquent relationship model

--- Framework Laravel x Vue.js/library/app/Http/Controllers/HomeController.php
+++ Framework Laravel x Vue.js/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\Book;
+use App\Models\Publisher;
+use App\Models\Author;
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        //$books = Book::with('author')->get();
+        //$books = Book::with('publisher')->get();
+        //$books = Book::with('catalog')->get();
+
+        //$publishers = Publisher::with('books')->get();
+        //$catalogs = Catalog::with('books')->get();
+        //$authors = Author::with('books')->get();
+
+        //return $catalogs;
         return view('home');
     }
 }
