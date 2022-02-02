ed8a33ca7ce5d7321736d26f91952bc4207804e0
doniarifin <muh.dhoni@gmail.com>
2022-02-03 00:40:45 +0700

    task compact data

--- Framework Laravel x Vue.js/library/resources/views/admin/catalog/index.blade.php
+++ Framework Laravel x Vue.js/library/resources/views/admin/catalog/index.blade.php
@@ -5,74 +5,44 @@
 @section('content')
 
 <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">Catalog</h3>
-              </div>
+    <div class="card-header">
+      <h3 class="card-title">Data Catalog</h3>
+    </div>
               <!-- /.card-header -->
-              <div class="card-body">
-                <table class="table table-bordered">
-                  <thead>
-                    <tr>
-                      <th style="width: 10px">#</th>
-                      <th>Task</th>
-                      <th>Progress</th>
-                      <th style="width: 40px">Label</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1.</td>
-                      <td>Update software</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-danger">55%</span></td>
-                    </tr>
-                    <tr>
-                      <td>2.</td>
-                      <td>Clean database</td>
-                      <td>
-                        <div class="progress progress-xs">
-                          <div class="progress-bar bg-warning" style="width: 70%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-warning">70%</span></td>
-                    </tr>
-                    <tr>
-                      <td>3.</td>
-                      <td>Cron job running</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-primary" style="width: 30%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-primary">30%</span></td>
-                    </tr>
-                    <tr>
-                      <td>4.</td>
-                      <td>Fix and squish bugs</td>
-                      <td>
-                        <div class="progress progress-xs progress-striped active">
-                          <div class="progress-bar bg-success" style="width: 90%"></div>
-                        </div>
-                      </td>
-                      <td><span class="badge bg-success">90%</span></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <!-- /.card-body -->
-              <div class="card-footer clearfix">
-                <ul class="pagination pagination-sm m-0 float-right">
-                  <li class="page-item"><a class="page-link" href="#">«</a></li>
-                  <li class="page-item"><a class="page-link" href="#">1</a></li>
-                  <li class="page-item"><a class="page-link" href="#">2</a></li>
-                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                  <li class="page-item"><a class="page-link" href="#">»</a></li>
-                </ul>
-              </div>
-            </div>
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th style="width: 10px">#</th>
+            <th class='text-center'>Name</th>
+            <th class='text-center'>Total Books</th>
+            <th class='text-center'>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($catalogs as $key => $catalog)
+          <tr>
+            <td> {{ $key+1 }} </td>
+            <td> {{ $catalog->name }} </td>
+            <td> {{ count($catalog->books) }} </td>
+            <td class='text-center'> {{ date('H:i:s - d M Y', strtotime($catalog->created_at)) }} </td>
+              
+          
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!-- /.card-body -->
+     <!-- <div class="card-footer clearfix">
+      <ul class="pagination pagination-sm m-0 float-right">
+        <li class="page-item"><a class="page-link" href="#">«</a></li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item"><a class="page-link" href="#">»</a></li>
+      </ul>
+    </div> -->
+  </div>
 
 @endsection
